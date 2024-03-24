b8a7a5c865ad3c26174412b2f100a7a9f6f02986
Brendan Joziasse <brendan.joziasse@gmail.com>
2024-03-24 09:20:29 +0100

    update

--- flask_app.py
+++ flask_app.py
@@ -1,1 +1,10 @@
- 
+ # Import flask-package
+from flask import Flask
+
+# create variable
+app=Flask(__name__)
+
+# Create default route
+@app.route('/')
+def home():
+    return "<p>Huiswerk les 11, vraag 6</p>"
